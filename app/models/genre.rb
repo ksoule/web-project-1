be7074e9572d905c2c35feb0f890c491cbdbd591
ksoule <ksoule77@gmail.com>
2018-03-23 12:05:15 -0500

    corrected associations

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,4 @@
 class  Genre < ApplicationRecord
-belongs_to :movies
+has_and_belongs_to_many :movies
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ApplicationRecord
   has_many :crew_positions
   has many :crew, through: 'crew_positions'
   has_many :actors, through: 'roles'
-  has_many :genres
+  has_and_belongs_to_many :genres
   has_many :reviews
 
 end
